0704bd4c7a67cee453cad10f1236a862490b8c52
raghav20 <prash.raaghav@gmail.com>
2014-05-25 18:08:56 -0400

    Update StopWordRemoval.py

--- StopWordRemoval.py
+++ StopWordRemoval.py
@@ -1,45 +1,31 @@
 import csv
 import re
 import Stemmer
-
+#Stemmer using nltk
 from nltk import PorterStemmer
-
+#select column from CSV
 def col_selector(table, column_key):
     return [row[column_key] for row in table]
 
+//Createfile
 def create_file(rest):
     file = open("Data/out.txt", "w")
     for item in rest:
         file.write("%s\n" % item)
 
 
-with open("Data/next.csv","r") as csvfile:
+with open("Data/Tweet.csv","r") as csvfile:
     reader = csv.DictReader(csvfile, delimiter=",")
     table = [row for row in reader]
-    foo_col = col_selector(table, "Summary")
-    bar_col = col_selector(table, "Description")
+    foo_col = col_selector(table, "User Name")
+    bar_col = col_selector(table, "Tweet")
 
-with open("Data/great.csv","r") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=",")
-    table = [row for row in reader]
-    new_col = col_selector(table, "Summary")
-    old_col = col_selector(table, "Description")
 
-with open("Data/Third.csv","r") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=",")
-    table = [row for row in reader]
-    def_col = col_selector(table, "Summary")
-    nde_col = col_selector(table, "Description")
 
-file = open("Data/c.txt", "w")
-for item in old_col:
-    file.write("%s\n" % item)
+# Tag Generation For Tweet
 
 lines = [line.strip() for line in open('Data/Stop/stop.txt')]
 
-file = open("Data/a.txt", "w")
-for item in bar_col:
-    file.write("%s\n" % item)
 
 file = open("Data/b.txt", "w")
 for item in foo_col:
@@ -49,12 +35,7 @@ for item in new_col:
 file.close()
 
 
-file = open("Data/e.txt", "w")
-for item in def_col:
-    file.write("%s\n" % item)
-for item in nde_col:
-    file.write("%s\n" % item)
-
+# Remove Stop Words from Tweets
 total =[]
 summ = []
 rest = []
@@ -62,6 +43,7 @@ linef = [line.strip() for line in open('Data/Stop/stop.txt')]
 linest = [line.strip() for line in open('Data/Stop/stop2.txt')]
 bad_characters = ["/","{","}","\\", ":", "(", ")", "<", ">", "|", "?", "*", ".", "#","&","'","-","$",",","~","[","]",";","@","`","_"]
 
+# Remove Special Charectors
 def check_word(word):
     word = word.lower()
     if len(word) > 0:
@@ -77,21 +59,8 @@ def check_word(word):
             if word not in linest:
                 rest.append(word)
 
-with open ("Data/a.txt") as f:
-  lines = f.readlines()
-  for line in lines:
-      words = line.split()
-      for word in words:
-          total.append(word)
-
-with open ("Data/b.txt") as f:
-  lines = f.readlines()
-  for line in lines:
-      words = line.split()
-      for word in words:
-          total.append(word)
-
-with open ("Data/c.txt") as f:
+# get final list with tags.
+with open ("Data/train1.txt") as f:
   lines = f.readlines()
   for line in lines:
       words = line.split()
@@ -100,7 +69,7 @@ with open ("Data/c.txt") as f:
 
 
 
-with open ("Data/e.txt") as f:
+with open ("Data/train2.txt") as f:
   lines = f.readlines()
   for line in lines:
       words = line.split()
@@ -113,6 +82,7 @@ for word in total:
     create_file(rest)
     print(len(rest))
 
+# Write tags into file.
 ifile = open("Data/out.txt","r")
 linelist = ifile.readlines()
 linelist.sort()
@@ -134,12 +104,5 @@ for line in open("Data/out.txt", "r"):
 outfile.close()
 
 
-import glob
 
-read_files = glob.glob("Data/Stop/*.txt")
-
-with open("Data/result.txt", "wb") as outfile:
-    for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(infile.read())
 
